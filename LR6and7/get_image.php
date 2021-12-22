e42d89870e31bf3970f1d36fd13bf96c7e0517a3
Uliasss <90374905+Uliasss@users.noreply.github.com>
2021-12-22 18:10:11 +0300

    Delete LR6and7 directory

--- LR6and7/get_image.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    session_start();
-    if($_SESSION['user']){ //защита картинок
-        header('Content-type: image/jpeg');
-        imagejpeg(imagecreatefromjpeg('images/'. $_GET['img']));
-    }else{
-        header('Location: index.php');
-    }
-?>
